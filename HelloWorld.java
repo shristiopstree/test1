6f18986c08c4702c269f9b6a1d9f485de5766773
shristi2400 <shristigupta313@gmail.com>
2024-08-03 23:13:35 +0530

    updated code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Arjun ji!");
+        System.out.println("Hello, Priyesh ji!");
     }
 }
 
